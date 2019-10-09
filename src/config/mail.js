c4b1579df829b854c0e4b50b80e43ae75663b38b
Diego Santos <diegoluizcsti@gmail.com>
2019-10-09 20:35:39 -0300

    instalendo e configurando nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'b62cda7c6e955f',
+    pass: '21fb3769409a6b',
+  },
+  default: {
+    from: 'Equipe BluesMan <noreplay@bluesman.com>',
+  },
+};
