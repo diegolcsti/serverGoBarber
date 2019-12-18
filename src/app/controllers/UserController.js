11a42c009c9df96b1d348a76cda99615e3ccd319
Diego Luiz <diegoluizcsti@gmail.com>
2019-12-18 09:27:53 -0300

    adicionando avatar ao update de UserController

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,5 +1,6 @@
 import * as Yup from 'yup';
 import User from '../models/User';
+import File from '../models/File';
 
 class UserController {
   async store(req, res) {
@@ -64,13 +65,24 @@ class UserController {
       return res.status(401).json({ error: 'password does not match' });
     }
 
-    const { id, name, provider } = await user.update(req.body);
+    await user.update(req.body);
+
+    const { id, name, provider, avatar } = await User.findByPk(req.userId, {
+      include: [
+        {
+          model: File,
+          as: 'avatar',
+          attributes: ['id', 'path', 'url'],
+        },
+      ],
+    });
 
     return res.json({
       id,
       name,
       email,
       provider,
+      avatar,
     });
   }
 }
